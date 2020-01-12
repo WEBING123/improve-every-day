0170a26a63c19d470aeb3bbe43682f92c007ca08
WEBING123 <123webing@gmail.com>
2020-01-12 22:11:34 +0800

    2020-1-12 22:11:26

--- vue/study-vue/src/App.vue
+++ vue/study-vue/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ComponentCarousel :percentage="10"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ComponentCarousel from './components/ComponentCarousel.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+    ComponentCarousel
   }
 }
 </script>
@@ -21,8 +23,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vue/study-vue/src/components/ComponentCarousel.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="progress">
+      <div class="progress-bar">
+          <div class="progress-bar__outer" :style="{height: strokeWidth + 'px'}">
+              <div class="progress-bar__inner" :style="{width: percentage + '%', backgroundColor: stroke}"></div>
+          </div>
+      </div>
+      <div class="progress-text" :style="{fontSize: progressTextSize + 'px'}">
+          90%
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentCarousel',
+  data() {
+      return {
+
+      }
+  },
+  props: {
+    strokeWidth: {
+        type: Number,
+        default: 6
+    },
+    percentage: {
+        type: Number,
+        default: 0,
+        required: true,
+        validator(value) {
+            return value >= 0 && value <= 100;
+        }
+    },
+    status: {
+        type: String
+    }
+  },
+  computed: {
+      progressTextSize () {
+          return 12 + this.strokeWidth * 0.4;
+      },
+      stroke() {
+          let color;
+          switch (this.status) {
+              case 'scuess':
+                  color = '#13ce66'
+                  break;
+          case 'exeption':
+              color = '#ff4949'
+              break;
+              default:
+                  color = '#20a0ff'
+          }
+          return color;
+      }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .progress-bar {
+        box-sizing: border-box;
+        display: inline-block;
+        width: 100%;
+        padding-right: 50px;
+        margin-right: -50px;
+    }
+    .progress-bar__outer {
+        border-radius: 100px;
+        background-color: #ebeef5
+    }
+    .progress-bar__inner {
+        height: 100%;
+        border-radius: 100px;
+        background-color: #ff4757;
+        transition: width .6s ease;
+    }
+    .progress-text {
+        display: inline-block;
+        margin-left: 10px;
+        color: #606266
+    }
+</style>
