99496a77babb353ffa1380ab89d795b8e6f98f5c
WEBING123 <123webing@gmail.com>
2020-01-12 16:08:16 +0800

    2020-1-12 16:08:11

---
+++ vue/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{ time }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            time: '2020-1-12 14:22:13'
+        }
+    }
+}
+</script>
