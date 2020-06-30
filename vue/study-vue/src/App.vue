3a6ce1a00c0ca45ee987af600b12f2d7e6f30664
WEBING123 <123webing@gmail.com>
2020-06-30 23:31:45 +0800

    2020-6-30 23:31:39

--- vue/study-vue/src/App.vue
+++ vue/study-vue/src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <ComponentCarousel :percentage="50"/>
-    <ComponentCarousel :percentage="80" status="success"/>
-    <ComponentCarousel :percentage="40" status="exeption"/>
+      <ElementTable></ElementTable>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import ComponentCarousel from './components/ComponentCarousel.vue'
+import ElementTable from './components/ElementTable.vue'
 
 export default {
   name: 'app',
   components: {
-    // HelloWorld
-    ComponentCarousel
+    ElementTable
   }
 }
 </script>
@@ -29,14 +24,6 @@ body {
     overflow: hidden;
 }
 #app {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 90%;
-    padding: 10px;
-    border: 2px dotted #f39c12;
-    border-radius: 5px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ vue/study-vue/src/AppComponentCarouselBackup.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ComponentCarousel :percentage="50"/>
+    <ComponentCarousel :percentage="80" status="success"/>
+    <ComponentCarousel :percentage="40" status="exeption"/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ComponentCarousel from './components/ComponentCarousel.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+    ComponentCarousel
+  }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+body {
+    overflow: hidden;
+}
+#app {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 90%;
+    padding: 10px;
+    border: 2px dotted #f39c12;
+    border-radius: 5px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
